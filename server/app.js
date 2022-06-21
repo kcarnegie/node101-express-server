12e9669b9891754d8108ddc7a2486345e9595ec3
kcarnegie <kevincarnegie@yahoo.com>
2022-06-21 13:31:41 -0700

    Adding initial files

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 // import files and packages up here
-
-
+const express = require('express');
+const morgan = require('morgan');
+const data = require('../server/data.json');
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
 
+app.get('/data', function(req, res){
+    res.json(data);
+})
+
+app.get('/', function(req, res) {
+    res.status(200).send('Hello');
+});
+
+app.get('*', function(req, res){
+    res.send('Not Found');
+});
 // finally export the express application
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // here we're importing your express app
-const server = require('./app');
+
+const server = require('./app')
 
 // We're telling the app to listen for network requests on port 3000
 server.listen(3000, function() {
